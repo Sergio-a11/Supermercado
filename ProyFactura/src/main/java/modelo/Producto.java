40c8fca51b1181ff4e4f919c4d6c4979a2f2a702
Sergio Cruz <cz.andrs@gmail.com>
2020-11-03 10:13:37 -0500

    Producto Clase Padre

--- ProyFactura/src/main/java/modelo/Producto.java
+++ ProyFactura/src/main/java/modelo/Producto.java
@@ -9,6 +9,63 @@ package modelo;
  *
  * @author Usuario
  */
-public class Producto {
+public abstract class Producto {
+    protected String cod,nombre;
+    protected double precioU;
+    protected int cantidad;
+
+    public Producto(String cod, String nombre, double precioU, int cantidad) {
+        this.cod = cod;
+        this.nombre = nombre;
+        this.precioU = precioU;
+        this.cantidad = cantidad;
+    }
     
+    public Producto() {
+        this.cod = "";
+        this.nombre = "";
+        this.precioU = 0;
+        this.cantidad = 0;
+    }
+
+    public String getCod() {
+        return cod;
+    }
+
+    public void setCod(String cod) {
+        this.cod = cod;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecioU() {
+        return precioU;
+    }
+
+    public void setPrecioU(double precioU) {
+        this.precioU = precioU;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+    
+    
+    public abstract double iva();
+
+    public double valor()
+    {
+        return this.precioU * this.cantidad;
+    }
 }
