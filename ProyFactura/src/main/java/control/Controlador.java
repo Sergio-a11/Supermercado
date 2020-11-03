e76ea3f100db465dc4acef0599ac22bb69089ffd
Sergio Cruz <cz.andrs@gmail.com>
2020-11-03 18:15:07 -0500

    oops

--- ProyFactura/src/main/java/control/Controlador.java
+++ ProyFactura/src/main/java/control/Controlador.java
@@ -31,12 +31,7 @@ public class Controlador {
         listaF.getListado().add(new Factura(objCM.leerTexto("Digite numero de la Factura"),
                                         new Fecha(objCM.leerEntero("Digite fecha\nDia:"),objCM.leerEntero("Mes[numero]:"),objCM.leerEntero("Año:")),
                                         new Cliente(objCM.leerTexto("Digite codigo del cliente:"),objCM.leerTexto("Digite nombre del cliente:"),objCM.leerTexto("Digite telefono del cliente:")),
-                
-<<<<<<< HEAD
                                         new ArrayList<Producto>()));
-=======
-                                        new ArrayList<>()));
->>>>>>> master
         do{
         Producto p = nuevoProducto();    
         listaF.getListado().get(aux).getProductos().add(p);
